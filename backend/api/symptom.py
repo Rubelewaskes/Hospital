6bda7fba56bbf2d61c882b64fadbaabc69f21c5d
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-12-04 15:06:25 +0300

    Докинул параметры _limit и _page на нужные запросы

--- backend/api/symptom.py
+++ backend/api/symptom.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, Response
 
 from api.dependencies import symptom_service
 from services import SymptomService
@@ -13,8 +13,18 @@ router = APIRouter(
 @router.get("/get_symptoms")
 async def get_all_symptoms(
     symptom_service: Annotated[SymptomService, Depends(symptom_service)],
+    response: Response,
+    _limit: int = Query(0, ge=0),
+    _page: int = Query(1, ge=1),
 ):
     symptoms = await symptom_service.get_all_symptoms()
+    response.headers["X-Total-Count"] = str(len(symptoms))
+
+    if _limit != 0:
+        start = (_page - 1) * _limit
+        end = start + _limit
+
+        return symptoms[start:end]
     return symptoms
 
 @router.post("/add_new")
