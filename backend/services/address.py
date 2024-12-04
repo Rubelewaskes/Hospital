954b940352d6a8f85d94a42389954b81662bce35
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-12-04 12:56:56 +0300

    Исправлена ошибка получения id введённого адреса

--- backend/services/address.py
+++ backend/services/address.py
@@ -7,7 +7,7 @@ class AddressService:
     def __init__(self, address_repo: AbstractRepository):
         self.address_repo: AbstractRepository = address_repo()
     
-    async def get_address_id(self, address_data):
+    async def get_address_id(self, data):
         address_info = AddressArea(street=data.street, house=data.house, 
             building=data.building, flat=data.flat)
         if address_id := await self.address_repo.find_address(address_info):
