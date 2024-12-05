46942c22e00e98876710c3710dfe42724d8d8e7a
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-12-05 10:22:21 +0300

    Добавлена возможность чтения заголовка x-total-count

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:1234@127.0.0.1:5432/hospital"
+DATABASE_URL = "postgresql+asyncpg://postgres:1234@postgrs_container:5432/hospital"
 
 engine = create_async_engine(url=DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
