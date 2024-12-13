49bed1c2c749e0601f4d92fca5b818edfa77c981
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-12-13 15:02:24 +0300

    Пагинация доктор

--- backend/api/doctor.py
+++ backend/api/doctor.py
@@ -12,9 +12,19 @@ router = APIRouter(
 
 @router.get("/get_all")
 async def get_all_doctors(
-    doctor_service: Annotated[DoctorService, Depends(doctor_service)],
+    doctor_service: Annotated[DoctorService, Depends(doctor_service)],\
+    response: Response,
+    _limit: int = Query(0, ge=0),
+    _page: int = Query(1, ge=1),
 ):
     all_doctors = await doctor_service.get_all_doctors()
+    response.headers["X-Total-Count"] = str(len(diagnoses))
+
+    if _limit != 0:
+        start = (_page - 1) * _limit
+        end = start + _limit
+
+        return diagnoses[start:end]
     return all_doctors
 
 @router.post("/add_new")
