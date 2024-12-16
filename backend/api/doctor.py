0fc02905f46a643831ebcd1397161cb173c11129
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-12-16 10:45:19 +0300

    Update doctor.py

--- backend/api/doctor.py
+++ backend/api/doctor.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, Response
 
 from api.dependencies import doctor_service
 from schemas import DoctorSchemaUpd, DoctorSchemaAdd
